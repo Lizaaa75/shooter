d7844fbcdad3433a748470099bef98065a87ef39
Lizaaa75 <lizakot862@gmail.com>
2025-04-13 15:56:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pygame import *
 from random import *
+from time import time as timer
 
 mixer.init()
 mixer.music.load('space (1).ogg')
@@ -13,15 +14,17 @@ win = font1.render('YOU WIN!', True, (255, 255, 255))
 lose = font1.render('YOU LOSE!', True, (255, 0, 0))
 font2 = font.Font(None, 36)
 
-img_back = "galaxy (1).jpg" #фон гри
-img_hero = "rocket (1).png" #гравець
-img_bullet = "bullet (1).png"
-img_enemy = "ufo (1).png" #ворог
+img_back = "background.jpg" #фон гри
+img_hero = "банка помідорів.png" #гравець
+img_bullet = "помідор-removebg-preview.png"
+img_enemy = "шахед.png" #ворог
+img_ast = "asteroid (1).png"
 
 score = 0
 lost = 0
 max_lost = 3
 goal = 10
+life = 3
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, x, y, w, h, player_speed): #конструктор
@@ -83,20 +86,34 @@ for i in range (1, 6):
         80, win_width - 80), -40, 80, 50, randint(1, 5)) # створюємо рандомно ворога
     monsters.add(monster) # додаємо в групу
 
+asteroids = sprite.Group()
+for i in range(1, 3):
+    asteroid = Enemy(img_ast, randint(30, win_width - 30), -40, 80, 50, randint(1, 7))
+    asteroids.add(asteroid)
+
 bullets = sprite.Group()
 
 finish = False
 run = True # прапорець скидається кнопкою закриття вікна
+rel_time = False
+num_fire = 0
 
 while run:
+    print(life)
     for e in event.get():
         if e.type == QUIT:
             run = False
 
         elif e.type == KEYDOWN: 
             if e.key == K_SPACE:
-                fire_sound.play()
-                ship.fire()
+                if num_fire < 10 and rel_time == False:
+                    num_fire = num_fire + 1
+                    fire_sound.play()
+                    ship.fire()
+
+                if num_fire >= 10 and rel_time == False:
+                    last_time = timer()
+                    rel_time = True
 
     if not finish:
         window.blit(background, (0, 0)) # фон
@@ -109,11 +126,24 @@ while run:
 
         ship.update() # оновлення гравця
         monsters.update() # оновлення списку воррогів 
+        asteroids.update()
         bullets.update()
 
         ship.reset() # відображення гравця
         monsters.draw(window) # відображення
+        asteroids.draw(window)
         bullets.draw(window)
+
+        if rel_time:
+            now_time = timer()
+
+            if now_time - last_time < 3:
+                reload = font2.render('А все, жди тепер, нічого було багато шмалять ...', 1, (150, 0, 0))
+                window.blit(reload, (100, 460))
+
+            else:
+                num_fire = 0
+                rel_time = False
 
         collides = sprite.groupcollide(monsters, bullets, True, True)
         for c in collides:
@@ -121,9 +151,35 @@ while run:
             monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
             monsters.add(monster)
 
-        if sprite.spritecollide(ship, monsters, False) or lost >= max_lost:
-            finish = True
+        #if sprite.spritecollide(ship, monsters, False) or lost >= max_lost:
+        #    finish = True
+        #    window.blit(lose, (200, 200))
+
+        '''
+        #можливий програш: пропустили занадто багато чи герой зіткнувся з ворогом чи встероїдом
+        if sprite.spritecollide(ship, monsters, False) or sprite.spritecollide(ship, asteroids, False):
+            finish = True # програли, ставимо тло і більше не керуємо спрайтами.
+            window.blit(lose, (200, 200))'''
+
+        if sprite.spritecollide(ship, monsters, False) or sprite.spritecollide(ship, asteroids, False):
+            sprite.spritecollide(ship, monsters, True)
+            sprite.spritecollide(ship, asteroids, True)
+            life = life - 1
+
+        if life == 0 or lost >= max_lost:
+            finish = True #програли, ставим фон і більше не упровляєм спрайтами
             window.blit(lose, (200, 200))
+
+        if life == 3:
+            life_color = (0, 150, 0)
+        if life == 2:
+            life_color = (150, 150, 0)
+        if life == 1:
+            life_color = (150, 0, 0)
+
+        text_life = font1.render(str(life), 1, life_color)
+        window.blit(text_life, (650, 10))
+
 
         if score >= goal:
             finish = True
